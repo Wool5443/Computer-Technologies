9eee7bdd8a2f377e43f17ccf2aba94e0707755f4
Wool5443 <mihsolodilov2015@gmail.com>
2024-12-07 18:06:31 +0300

    Parsing args

--- Task2/Runsim/src/RunSim.c
+++ Task2/Runsim/src/RunSim.c
@@ -1,13 +1,35 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
+
+#include "Vector.h"
 #include "RunSim.h"
 
 #define MAX_USER_INPUT 512
 
+static void sigchildHandler(int signum, siginfo_t* siginfo, void* unused);
+static const char** parseArgs(char string[static 1]);
+
+size_t running = 0;
+
 ErrorCode RunSim(size_t maxPrograms)
 {
     ERROR_CHECKING();
+
+    const char** args = NULL;
+
+    struct sigaction sa = {};
+    sa.sa_flags = SA_SIGINFO | SA_RESTART;
+    sa.sa_sigaction = sigchildHandler;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        int ern = errno;
+        err = ERROR_LINUX;
+        LogError("sigaction failed: %s", strerror(ern));
+        ERROR_LEAVE();
+    }
 
     char userInput[MAX_USER_INPUT + 1] = "";
     size_t running = 0;
@@ -26,8 +48,15 @@ ErrorCode RunSim(size_t maxPrograms)
             goto nextLoop;
         }
 
+        args = parseArgs(userInput);
+
+        if (!args)
+        {
+            goto nextLoop;
+        }
+
         pid_t pid = fork();
-        if (pid < 0)
+        if (pid == -1)
         {
             int ern = errno;
             err = ERROR_LINUX;
@@ -36,18 +65,17 @@ ErrorCode RunSim(size_t maxPrograms)
         }
         else if (pid == 0)
         {
-            char* args[] = { userInput, NULL };
-            execvp(userInput, args);
+            execvp(userInput, (char**)args);
 
             int ern = errno;
             err = ERROR_LINUX;
             LogError("execvp error: %s", strerror(ern));
             ERROR_LEAVE();
         }
-        else
-        {
-            running++;
-        }
+
+        running++;
+
+        VecDtor(args);
 
 nextLoop:
         while (running > 0)
@@ -55,20 +83,20 @@ nextLoop:
             int status = 0;
             pid_t finishedPid = waitpid(-1, &status, WNOHANG);
 
-            if (finishedPid > 0)
+            if (finishedPid == -1)
+            {
+                int ern = errno;
+                err = ERROR_LINUX;
+                LogError("waitpid error: %s", strerror(ern));
+                ERROR_LEAVE();
+            }
+            else if (finishedPid > 0)
             {
                 running--;
             }
             else if (finishedPid == 0)
             {
                 break;
-            }
-            else
-            {
-                int ern = errno;
-                err = ERROR_LINUX;
-                LogError("waitpid error: %s", strerror(ern));
-                ERROR_LEAVE();
             }
         }
     }
@@ -80,5 +108,45 @@ nextLoop:
     }
 
 ERROR_CASE
+    VecDtor(args);
     return err;
+}
+
+static void sigchildHandler(int signum, siginfo_t* siginfo, void* unused)
+{
+
+}
+
+static const char** parseArgs(char string[static 1])
+{
+    ERROR_CHECKING();
+
+    assert(string);
+
+    const char** args = NULL;
+
+    char* current = strtok(string, " ");
+
+    while (current)
+    {
+        if ((err = VecAdd(args, current)))
+        {
+            LogError("Error parsing args");
+            ERROR_LEAVE();
+        }
+        current = strtok(NULL, " ");
+    }
+
+    if ((err = VecAdd(args, NULL)))
+    {
+        LogError("Error parsing args");
+        ERROR_LEAVE();
+    }
+
+    return args;
+
+ERROR_CASE
+    VecDtor(args);
+
+    return NULL;
 }
